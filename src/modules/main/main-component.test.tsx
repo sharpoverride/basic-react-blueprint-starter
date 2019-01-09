f81ae032d26a6bcc75aa51f3f91b5b9bd0e2203d
Mihai Lazar <Mihai.Lazar@ascom.com>
2019-01-09 16:33:10 +0200

    Add test debug support and the class-validator library

--- src/modules/main/main-component.test.tsx
+++ src/modules/main/main-component.test.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter} from "react-router-dom";
 import { Layout } from '../../components/layout';
 import MainRoute from './';
 
+import {validate, Length} from 'class-validator';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<BrowserRouter>
@@ -13,4 +15,20 @@ it('renders without crashing', () => {
     </Layout>
 </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+class Demo {
+  @Length(0,2, {message: "You should not have a title with more than 2 characters."})
+  public title: string = '';
+}
+it('validates my class', async (done) => {
+
+  const d = new Demo();
+  d.title='abc';
+
+  const results = await validate(d);
+
+  console.log('bce', results[0].constraints.length);
+
+  done();
 });
